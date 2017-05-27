651c6908aadad6aff6693eda2c380d14cc47e0f2
ErwinSalas <erwinsalas42@gmail.com>
2017-05-27 15:50:50 -0600

    first commit

---
+++ app/src/main/java/com/example/pavilion/designprojectll/Properties/IndexPropertyActivity.java
@@ -0,0 +1,15 @@
+package com.example.pavilion.designprojectll.Properties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pavilion.designprojectll.R;
+
+public class IndexPropertyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_property);
+    }
+}
